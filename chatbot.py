d09830cfc8a1d68dbb3e292a0510086f278fb388
ShubhangDesai <shubhang.desai@gmail.com>
2018-03-02 10:48:40 -0800

    add multiple movie sentiment detection

--- chatbot.py
+++ chatbot.py
@@ -165,7 +165,7 @@ class Chatbot:
         first_quote = input.find('\"') + 1
         second_quote = first_quote + input[first_quote:].find('\"')
         movie = input[first_quote:second_quote]
-        input = input[:first_quote-2] + input[second_quote+1:]
+        input = input[:first_quote-1] + input[second_quote+1:]
         return movie, input
 
     def rearrageArt(self, movie, date):
@@ -386,10 +386,10 @@ class Chatbot:
                     orig_movies.append(orig_movie)
                     dates.append(None)
                     sentiments.append(0.0)
-                if (not input or not movie) and (not self.genState == "CLARIFY" or not self.is_continuation(input)):
-                    break
+                # if (not input or not movie) and (not self.genState == "CLARIFY" or not self.is_continuation(input)):
+                #     break
 
-                joins = [' and ', ' but ']
+                joins = [' and ', ' but ', ' or ']
                 first_join, first_idx = '', float('Inf')
                 for join in joins:
                     idx = input.find(join)
@@ -399,8 +399,23 @@ class Chatbot:
                 clauses = [input] if first_join == '' else input.split(first_join)
                 input = '' if first_join == '' else first_join.join(clauses[1:])
 
-                pos_neg_count = self.extract_sentiment(clauses[0])
-                #print(clauses, pos_neg_count, movie)
+                features = clauses[0]
+                print(features, filter(str.isalnum, features))
+                if filter(str.isalnum, features) == '':
+                    movies.append(movie)
+                    sentiments.append(sentiments[-1])
+                    dates.append(date)
+                    orig_movies.append(orig_movie)
+                    continue
+                elif filter(str.isalnum, features) == 'not':
+                    movies.append(movie)
+                    sentiments.append(-1 if sentiments[-1] == 1 else 1)
+                    dates.append(date)
+                    orig_movies.append(orig_movie)
+                    continue
+
+
+                pos_neg_count = self.extract_sentiment(features)
                 movies.append(movie)
                 sentiments.append(pos_neg_count)
                 dates.append(date)
